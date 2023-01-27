ec0c0ae9315dadc71961d02ed55d6f4d3d6a6b75
Fabian Vilers <fabian.vilers@dev-one.com>
2023-01-27 09:41:58 +0100

    Add key listening using win32 API

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::{thread, time};
+
+#[cfg(windows)]
+extern crate winapi;
+
+fn is_pressed(virtual_key: &i32) -> bool {
+    use winapi::um::winuser::GetAsyncKeyState;
+
+    let key_state = unsafe { GetAsyncKeyState(*virtual_key) };
+    let key_state = key_state as u32 >> 31;
+
+    key_state == 1
+}
+
+#[cfg(windows)]
+fn listen_for_keys(on_key_pressed: fn(i32)) {
+    loop {
+        // TODO: exclude mouse buttons
+        // https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
+        (0..255)
+            .into_iter()
+            .filter(is_pressed)
+            .for_each(on_key_pressed);
+
+        thread::sleep(time::Duration::from_millis(100));
+    }
+}
+
+#[cfg(not(windows))]
+fn listen_for_keys() {
+    panic!("OS not supported");
+}
+
 fn main() {
-    println!("Hello, world!");
+    listen_for_keys(|key| println!("{key} pressed"));
 }
